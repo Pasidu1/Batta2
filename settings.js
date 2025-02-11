9f221039e372b55ee34e06940e3bf096b4a007d7
Pasidu1 <163862208+Pasidu1@users.noreply.github.com>
2025-02-11 20:41:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6uZFHCxL#kIstzGVKRLsluFk9sTjlvYXD73tx0RgLP01rU02EzHw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=u7420Ija#HnnY8W-oyHFffGLs3-Pedb4hHdlNlNuVo3KTVXtLVFQ' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
